b4dcee430b29d4695ca1bdf466aaca827d9b7244
ggrandes <guillermo.grandes@gmail.com>
2022-03-06 18:53:20 +0100

    Config: Improve trace read config error

--- src/main/java/org/javastack/tinyurl/Config.java
+++ src/main/java/org/javastack/tinyurl/Config.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
 import org.javastack.mapexpression.InvalidExpression;
 import org.javastack.stringproperties.StringProperties;
 
 public class Config {
+	static final Logger log = Logger.getLogger(Config.class);
 	private static final String PACKAGE = Config.class.getPackage().getName();
 	public static final String PROP_CONFIG = PACKAGE + ".config";
 	public static final String DEF_CONFIG_FILE = PACKAGE + ".properties";
@@ -62,8 +64,14 @@ public class Config {
 		InputStream is = null;
 		if (fileName.startsWith("http:") || fileName.startsWith("https:") || fileName.startsWith("file:")) {
 			is = new URL(fileName).openStream();
+			if (is == null) {
+				log.error("Unable to read config file (URL): " + fileName);
+			}
 		} else {
 			is = getClass().getResourceAsStream("/" + fileName);
+			if (is == null) {
+				log.error("Unable to read config file (classpath): " + fileName);
+			}
 		}
 		return is;
 	}
